e030db4cb12cb3a971cc957659cdafcc41512540
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-04-20 10:27:01 +0200

    fix erroneous exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { useRedux } from './useRedux'
 export { useReduxState } from './useReduxState'
 export { useReduxDispatch } from './useReduxDispatch'
-export { useReduxActions, Parameters, ActionsMap, BoundActionsMap } from './useReduxActions'
+export { useReduxActions } from './useReduxActions'
